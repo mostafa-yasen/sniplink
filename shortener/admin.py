c40f5c20591065a5d78acaa92e49b3140fcc2f2f
Mostafa Yasin <mostafa.a.yasin@gmail.com>
2025-09-24 23:47:54 +0300

    feat: add ShortenedUrl model admin

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,2 +1,7 @@
-# from django.contrib import admin
-# Register your models here.
+from django.contrib import admin
+from .models import ShortenedURL
+
+
+@admin.register(ShortenedURL)
+class ShortenedURLAdmin(admin.ModelAdmin):
+    ...
